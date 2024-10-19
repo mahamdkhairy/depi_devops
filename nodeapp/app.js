09c454d1bda42f2376cf69426af3b32c2b2ed935
mahamdkhairy <Mahamd.khairy@gmail.com>
2024-10-19 20:02:12 +0300

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
-    res.send('Hello World from Mohamed Khairy!')
-  })
-  
+  res.send('Hello World from Mohamed Khairy!');
+});
+
+if (require.main === module) {
   app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+}
+
+module.exports = app;
