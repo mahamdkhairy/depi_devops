75f344fc15c9b1a38001cf89feb7013b62060aa9
mahamdkhairy <Mahamd.khairy@gmail.com>
2024-10-23 17:17:18 +0300

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Mohamed Khairy!!');
+  res.send('Hello World from Mohamed Khairy!');
 });
 
 if (require.main === module) {
